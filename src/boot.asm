a64145fdf67ee7401a0e30d8e8d66dbdcd19b003
Tamir Hadash <udbe3333@gmail.com>
2023-11-06 22:33:06 +0200

    hello world operating system

--- src/boot.asm
+++ src/boot.asm
@@ -1,3 +1,50 @@
 org 0x7c00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+puts:
+    push si
+    push ax
+
+.loop:
+    lodsb
+    or al,al
+    jz .done
+
+    mov bh,0
+    mov ah, 0x0e
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
+
+main:
+    ;setup data segments
+    xor ax,ax
+    mov ds,ax
+    mov es,ax
+    
+    ;setup stack
+    mov ss,ax
+    mov sp,0x7c00 ;start of the program because stacks grows downwords
+
+    mov si,msg_hello_world
+    call puts
+
+
+    hlt
+.halt:
+    jmp .halt
+
+
+
+msg_hello_world: db 'Hello World!',ENDL , 0
+
+times 510-($-$$) db 0
+dw 0AA55h
